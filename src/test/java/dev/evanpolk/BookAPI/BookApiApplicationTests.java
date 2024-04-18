52c54042aa35c73bd45b96f7ef7c94f35ad44476
Evan Polk <63527030+EvanPolk@users.noreply.github.com>
2024-04-18 16:33:38 -0500

    start.spring.io initializer
    Dependencies include Spring web, Spring configuration processor, Spring for GraphQL, and Docker compose support.

---
+++ src/test/java/dev/evanpolk/BookAPI/BookApiApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.evanpolk.BookAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
